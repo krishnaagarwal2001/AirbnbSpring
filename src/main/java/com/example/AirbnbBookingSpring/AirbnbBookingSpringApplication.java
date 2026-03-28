72165a5ea5c3084d491f1edefbaabd2048e382a3
krishna <krishna_a@ce.iitr.ac.in>
2026-03-28 16:35:26 +0530

    Taken configurable properties from .env

--- src/main/java/com/example/AirbnbBookingSpring/AirbnbBookingSpringApplication.java
+++ src/main/java/com/example/AirbnbBookingSpring/AirbnbBookingSpringApplication.java
@@ -1,5 +1,6 @@
 package com.example.AirbnbBookingSpring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,7 +11,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 public class AirbnbBookingSpringApplication {
 
+    private static void loadEnvironmentVariables(){
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+
 	public static void main(String[] args) {
+        loadEnvironmentVariables();
+
 		SpringApplication.run(AirbnbBookingSpringApplication.class, args);
 	}
 
